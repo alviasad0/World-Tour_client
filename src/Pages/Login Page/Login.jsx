a8785d2cc652a357ee4ca3d35a673dd2e1a8f8c1
alviasad0 <alviasad10@gmail.com>
2023-11-05 02:25:29 +0600

    designd the login page

---
+++ src/Pages/Login Page/Login.jsx
@@ -0,0 +1,198 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useContext } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import auth from './../../firebase/firebase.config';
+import { FcGoogle } from "react-icons/fc";
+import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+
+const Login = () => {
+    const googleProvider = new GoogleAuthProvider();
+    const location = useLocation();
+    const navigate = useNavigate();
+    console.log(location);
+
+    const { logIn } = useContext(AuthContext);
+    const handleLogin = (event) => {
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        logIn(email, password)
+            .then((result) => {
+                Swal.fire({
+                    icon: "success",
+                    title: "Success!",
+                    text: `${result.user.email} have logged in successfully!`,
+                });
+                console.log(result);
+                navigate(location?.state ? location.state : "/");
+            })
+            .catch((error) => {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: `Password doesn't match`,
+                });
+                console.log(error.message);
+            });
+        console.log(email, password);
+    };
+
+
+
+    /* google login */
+    const handleGoogleLogin = () => {
+        signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                Swal.fire({
+                    icon: "success",
+                    title: "Success!",
+                    text: `${result.user.displayName} have logged in successfully!`,
+                });
+                navigate(location?.state ? location.state : "/");
+                console.log(result.user);
+            })
+            .catch((error) => {
+                Swal.fire({
+                    icon: "error",
+                    title: "Oops...",
+                    text: `${error.message}`,
+                });
+                console.log(error.message);
+            });
+        console.log("Google mama  coming");
+    };
+    return (
+        <div>
+           <div
+          data-aos="fade-down"
+          data-aos-easing="ease-out-cubic"
+          data-aos-duration="1500"
+          className="bg-green-50 container mx-auto md:p-10 mt-10 rounded-lg"
+        >
+          <div className="text-center ">
+            <h1
+              className="text-5xl font-bold pb-20 pt-10"
+              data-aos="fade-up"
+              data-aos-easing="linear"
+              data-aos-duration="3000"
+            >
+              Login now!
+            </h1>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-6  ">
+            <div
+              className="col-span-3"
+              data-aos="fade-right"
+              data-aos-easing="linear"
+              data-aos-duration="3000"
+            >
+              <form onSubmit={handleLogin} className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="text-xl text-black font-medium tracking-wider">
+                      Email
+                    </span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered input-success"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="text-xl text-black font-medium tracking-wider">
+                      Password
+                    </span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="input input-bordered input-success"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-success  tracking-widest text-2xl">
+                    Login
+                  </button>
+                </div>
+                <p className="text-lg text-black  font-medium">
+                  Do not have an account ?{" "}
+                  <Link
+                    to="/register"
+                    className="text-green-600 text-xl font-bold"
+                  >
+                    {" "}
+                    Register
+                  </Link>
+                </p>
+              </form>
+            </div>
+            <div
+              data-aos="fade-left"
+              data-aos-easing="linear"
+              data-aos-duration="3000"
+            >
+              <div>
+                <div>
+                  <p className="text-3xl font-bold text-center "> Login with</p>
+                  <div>
+                    <button
+                      onClick={handleGoogleLogin}
+                      className="mt-6 bg-green-100 btn w-full  border-green-600 space-x-3 tracking-widest"
+                    >
+                      <FcGoogle className="text-2xl"></FcGoogle>
+                      <span className="text-lg font-bold">Google</span>
+                    </button>
+                    <button className="mt-2 btn bg-green-100 w-full border-green-600 space-x-3 tracking-widest">
+                      <BsGithub className="text-2xl"></BsGithub>
+                      <span className="text-lg font-bold">Github</span>
+                    </button>
+                  </div>
+                </div>
+                <div className="mt-8 text-center">
+                  <h1 className="text-3xl font-bold">Find Us On</h1>
+                  <div className="border-green-400 border-2 mt-6">
+                    <div className=" border-green-400 border-b-2 p-4">
+                      <a
+                        href=""
+                        className="flex text-xl pl-10 font-semibold items-center gap-2"
+                      >
+                        <BsFacebook></BsFacebook>
+                        <span>FaceBook</span>
+                      </a>
+                    </div>
+                    <div className="border-green-400  border-b-2 p-4">
+                      <a
+                        href=""
+                        className="flex text-xl pl-10 font-semibold items-center gap-2"
+                      >
+                        <BsInstagram></BsInstagram>
+                        <span>Instagram</span>
+                      </a>
+                    </div>
+                    <div className="  p-4">
+                      <a
+                        href=""
+                        className="flex text-xl pl-10 font-semibold items-center gap-2"
+                      >
+                        <BsTwitter></BsTwitter>
+                        <span>Twitter</span>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,274 @@
+import { Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import auth from './../../firebase/firebase.config';
+import Swal from "sweetalert2";
+import { FcGoogle } from "react-icons/fc";
+import { BsFacebook, BsGithub, BsInstagram, BsTwitter } from "react-icons/bs";
+
+
+const Register = () => {
+     const navigate = useNavigate();
+     const { createUser, setUserName, setProfilePicture } =
+       useContext(AuthContext);
+    const googleProvider = new GoogleAuthProvider();
+    
+
+
+    const handleRegister = (event) => {
+      event.preventDefault();
+      const name = event.target.name.value;
+      const userPic = event.target.userPic.value;
+      const email = event.target.email.value;
+      const password = event.target.password.value;
+      const confirmPassword = event.target.password2.value;
+      if (password !== confirmPassword) {
+        return Swal.fire(
+          "Error!",
+          "Password and Confirm password  should same !",
+          "error"
+        );
+      } else if (password < 6) {
+        return Swal.fire(
+          "Error!",
+          "Password should be atleast 6 charecters long  !",
+          "error"
+        );
+      } else if (!/[A-Z]/.test(password)) {
+        return Swal.fire(
+          "Error!",
+          "Password should contain atleast one uppercase letter !",
+          "error"
+        );
+      } else if (!/[!@#$%^&*()_+{}[\]:;<>,.?~\\-]/.test(password)) {
+        return Swal.fire(
+          "Error!",
+          "Password should contain atleast one special charecter!",
+          "error"
+        );
+      }
+      createUser(email, password)
+        .then((result) => {
+          setUserName(name);
+          setProfilePicture(userPic);
+          Swal.fire({
+            icon: "success",
+            title: "Success!",
+            text: "You have registered successfully!",
+          });
+          console.log(result.user);
+        })
+        .catch((error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: `${error.message}`,
+          });
+          console.log(error.message);
+        });
+      console.log(name, userPic, email, password, confirmPassword);
+    };
+
+
+    /* google log in  */
+    const handleGoogleLogin = () => {
+      signInWithPopup(auth, googleProvider)
+        .then((result) => {
+          Swal.fire({
+            icon: "success",
+            title: "Success!",
+            text: "You have registered successfully!",
+          });
+          navigate(location?.state ? location.state : "/");
+          console.log(result.user);
+        })
+        .catch((error) => {
+          Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: `${error.message}`,
+          });
+          console.log(error.message);
+        });
+      console.log("glg mam  coming");
+    };
+
+    return (
+      <div>
+        
+        <div
+          data-aos="fade-down"
+          data-aos-easing="linear"
+          data-aos-duration="1500"
+          className="bg-green-50 container mx-auto md:p-10 mt-10 rounded-lg"
+        >
+          <div className="text-center ">
+            <h1 className="text-5xl text-black font-bold">Register now!</h1>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
+            <div
+              className="col-span-3"
+              data-aos="fade-right"
+              data-aos-easing="linear"
+              data-aos-duration="3000"
+            >
+              <form onSubmit={handleRegister} className="card-body">
+                <div className="form-control">
+                  <label className="label">
+                    <span className="text-xl text-black font-medium tracking-wider">
+                      Name
+                    </span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    className="input input-bordered input-success"
+                    name="name"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="text-xl text-black font-medium tracking-wider">
+                      Profile Picture URL
+                    </span>
+                  </label>
+                  <input
+                    type="text"
+                    placeholder="URL"
+                    className="input input-bordered input-success"
+                    name="userPic"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="text-xl text-black font-medium tracking-wider">
+                      Email
+                    </span>
+                  </label>
+                  <input
+                    type="email"
+                    placeholder="email"
+                    className="input input-bordered input-success"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="text-xl text-black font-medium tracking-wider">
+                      Password
+                    </span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="password"
+                    className="input input-bordered input-success"
+                    name="password"
+                    required
+                  />
+                </div>
+                <div className="form-control">
+                  <label className="label">
+                    <span className="text-xl text-black font-medium tracking-wider">
+                      Confirm Password
+                    </span>
+                  </label>
+                  <input
+                    type="password"
+                    placeholder="Confirm password"
+                    className="input input-bordered input-success"
+                    name="password2"
+                    required
+                  />
+                </div>
+                <div className="form-control mt-6">
+                  <button className="btn btn-success text-xl  font-bold tracking-widest">
+                    Register
+                  </button>
+                </div>
+                <p className="text-lg text-black  font-medium">
+                  Already have an account ?
+                  <Link
+                    to="/login"
+                    className="text-green-600 text-xl font-bold"
+                  >
+                    Login
+                  </Link>
+                </p>
+
+                <hr />
+              </form>
+            </div>
+            <div className="col-span-1">
+              <div>
+                <div
+                  data-aos="fade-left"
+                  data-aos-easing="linear"
+                  data-aos-duration="3000"
+                >
+                  <div>
+                    <div>
+                      <p className="text-3xl font-bold text-center ">
+                        {" "}
+                        Login with
+                      </p>
+                      <div>
+                        <button
+                          onClick={handleGoogleLogin}
+                          className="mt-6 btn bg-green-100 w-full  border-green-600 space-x-3 tracking-widest"
+                        >
+                          <FcGoogle className="text-2xl"></FcGoogle>
+                          <span className="text-lg font-bold">Google</span>
+                        </button>
+                        <button className="mt-2 btn bg-green-100 w-full border-green-600 space-x-3 tracking-widest">
+                          <BsGithub className="text-2xl"></BsGithub>
+                          <span className="text-lg font-bold">Github</span>
+                        </button>
+                      </div>
+                    </div>
+                    <div className="mt-8 text-center">
+                      <h1 className="text-3xl font-bold">Find Us On</h1>
+                      <div className="border-green-400 border-2 mt-6">
+                        <div className=" border-green-400 border-b-2 p-4">
+                          <a
+                            href=""
+                            className="flex text-xl pl-10 font-semibold items-center gap-2"
+                          >
+                            <BsFacebook></BsFacebook>
+                            <span>FaceBook</span>
+                          </a>
+                        </div>
+                        <div className="border-green-400  border-b-2 p-4">
+                          <a
+                            href=""
+                            className="flex text-xl pl-10 font-semibold items-center gap-2"
+                          >
+                            <BsInstagram></BsInstagram>
+                            <span>Instagram</span>
+                          </a>
+                        </div>
+                        <div className="  p-4">
+                          <a
+                            href=""
+                            className="flex text-xl pl-10 font-semibold items-center gap-2"
+                          >
+                            <BsTwitter></BsTwitter>
+                            <span>Twitter</span>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+    );
+};
+
+export default Register;
